91682fbc8f9bc4af8448a2c8e67d602db7836899
BahatiPrince07 <inezaremyprince5@gmail.com>
2026-03-30 10:18:03 +0200

    New frontend update

--- Frontend/Mt-Mariko/src/App.jsx
+++ Frontend/Mt-Mariko/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Signup from "./pages/Register";
+import Signup from "./pages/Signup"; // Make sure this file exists as Signup.jsx, not Register.jsx
 import Dashboard from "./pages/Dashboard";
 import Members from "./pages/Members";
 import CreateMember from "./pages/CreateMember";
@@ -18,19 +18,19 @@ import ReportDetails from "./pages/ReportDetails";
 import ProtectedRoute from "./components/ProtectedRoute";
 import FamilyView from "./components/FamilyView";
 import FamiliesList from "./components/FamiliesList";
+
 export default function App() {
   return (
     <Router>
       <Routes>
-        {/* Public Routes */}
+        {/* Public Routes - Everyone can access */}
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/events-public" element={<EventsPublic />} />
         <Route path="/events-public/:id" element={<EventDetailsPublic />} />
         
-        {/* Protected Routes */}
-
+        {/* Protected Routes - Require authentication */}
         <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
         <Route path="/members" element={<ProtectedRoute><Members /></ProtectedRoute>} />
         <Route path="/members/create" element={<ProtectedRoute><CreateMember /></ProtectedRoute>} />
@@ -42,8 +42,8 @@ export default function App() {
         <Route path="/reports" element={<ProtectedRoute><Reports /></ProtectedRoute>} />
         <Route path="/reports/:id" element={<ProtectedRoute><ReportDetails /></ProtectedRoute>} />
         <Route path="/admin/messages" element={<ProtectedRoute><AdminMessages /></ProtectedRoute>} />
-        <Route path="/members/:id/family" element={<ProtectedRoute><FamilyView /></ProtectedRoute> } />
-        <Route path="/families" element={<ProtectedRoute> <FamiliesList /> </ProtectedRoute>} />
+        <Route path="/members/:id/family" element={<ProtectedRoute><FamilyView /></ProtectedRoute>} />
+        <Route path="/families" element={<ProtectedRoute><FamiliesList /></ProtectedRoute>} />
       </Routes>
     </Router>
   );
